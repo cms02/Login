6f11f879bdfaa5ffa27eec0fcc3d93f7a9a80b67
cms02 <minoui1234@gmail.com>
2022-07-19 19:40:16 +0900

    [feat] 1. application.yml 설정 2. Member Entity 생성

---
+++ src/main/java/com/example/Login/entity/Member.java
@@ -0,0 +1,17 @@
+package com.example.Login.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+}

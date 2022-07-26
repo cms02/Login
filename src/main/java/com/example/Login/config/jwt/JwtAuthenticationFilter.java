61e5f7ae7fd2573376e65c1a701c090088c9bde8
cms02 <minoui1234@gmail.com>
2022-07-26 17:16:02 +0900

    로그인 성공 시 Response 값 ObjectMapper 로 Json Parsing

--- src/main/java/com/example/Login/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/Login/config/jwt/JwtAuthenticationFilter.java
@@ -2,12 +2,17 @@ package com.example.Login.config.jwt;
 
 import com.example.Login.config.auth.PrincipalDetails;
 import com.example.Login.dto.request.MemberRequestDto;
+import com.example.Login.dto.response.MemberResponseDto;
+import com.example.Login.dto.response.ResponseDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.codec.Utf8;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -15,20 +20,24 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     private final AuthenticationManager authenticationManager;
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        System.out.println("JwtAuthenticationFilter 진입");
 
-        ObjectMapper om = new ObjectMapper();
+        log.info("JwtAuthenticationFilter 진입");
+
         MemberRequestDto.Login loginDto = new MemberRequestDto.Login();
         try {
-            loginDto = om.readValue(request.getInputStream(), MemberRequestDto.Login.class);
+            loginDto = objectMapper.readValue(request.getInputStream(), MemberRequestDto.Login.class);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -49,9 +58,25 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
 
+        MemberResponseDto.Login memberResponseDto = MemberResponseDto.Login.builder()
+                .username(principalDetails.getUsername())
+                .message("LOGIN SUCCESS")
+                .build();
+
+        ResponseDto responseDto = ResponseDto.builder()
+                .data(memberResponseDto)
+                .status(HttpStatus.OK)
+                .build();
+
         String jwtToken = TokenUtil.generateToken(principalDetails.getMember());
 
         response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(responseDto));
+
+
+
 
     }
 }

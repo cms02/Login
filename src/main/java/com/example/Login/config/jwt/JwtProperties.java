2d4997526604583e01db0f4564c4c48c9e9505c6
cms <minoui1234@gmail.com>
2022-09-11 16:01:31 +0900

    RedisConfig 및 RedisService 생성

--- src/main/java/com/example/Login/config/jwt/JwtProperties.java
+++ src/main/java/com/example/Login/config/jwt/JwtProperties.java
@@ -2,7 +2,7 @@ package com.example.Login.config.jwt;
 
 public interface JwtProperties {
     String SECRET = "jWtSeCrEtKeY";
-    int EXPIRATION_TIME = 1000*60*30; //30mins
+    int EXPIRATION_TIME = 1000*60*5; //30mins
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
 }
